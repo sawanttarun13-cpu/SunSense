d72773bf03a7404b64d52319a4b48f63f6e95bd4
Tarun <sawanttarun13@gmail.com>
2026-09-01 11:53:20 +0530

    chore: cleanup old test and scratch scripts

--- backend/test_readings.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const latest = await prisma.exposureSession.findMany({ orderBy: { startTime: 'desc' }, take: 10 });
-  console.log(JSON.stringify(latest, null, 2));
-}
-main().finally(() => prisma.$disconnect());
